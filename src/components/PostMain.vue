a72dfe9a40f829e5ed2f68478b3211116717cd53
info-mic-info <jobsocial94@gmail.com>
2023-03-14 22:21:32 +0100

    Milestone 3

---
+++ src/components/PostMain.vue
@@ -0,0 +1,31 @@
+        <script>
+export default {
+  name: "PostMain",
+  data() {
+    return {
+      posts: [],
+      loading: true,
+      baseUrl: "http://127.0.0.1:8000",
+    };
+  },
+  methods: {
+    getPosts() {
+      this.loading = true;
+      axios.get(`${this.baseUrl}/api/posts`).then((response) => {
+        this.post = response.data.results;
+        this.loading = false;
+      });
+    },
+  },
+};
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
